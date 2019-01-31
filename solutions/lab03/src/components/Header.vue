fd529c5bf3c3071ad69b5fde65c629db11846f78
Chris Minnick <chris@minnick.com>
2019-01-31 15:47:58 -0800

    finished lab03

--- solutions/lab03/src/components/Header.vue
+++ solutions/lab03/src/components/Header.vue
@@ -1,15 +1,20 @@
 <template>
-<nav class="navbar navbar-default">
+<nav class="navbar">
    <div class="container">
       <a href="/" class="navbar-brand">Conduit</a>
-      <ul class="nav navbar-nav flex-row ">
-        <li class="nav-item p-2"><a class="nav-link">Home</a></li>
-        <li class="nav-item p-2"><a class="nav-link">Sign In</a></li>
-        <li class="nav-item p-2"><a class="nav-link">Sign Up</a></li>
+      <ul class="nav navbar-nav flex-row">
+        <li class="nav-item p-2">
+          <a class="nav-link">Home</a>
+        </li>
+        <li class="nav-item p-2">
+          <a class="nav-link">Sign In</a>
+        </li>
+        <li class="nav-item p-2">
+          <a class="nav-link">Sign Up</a>
+        </li>
       </ul>
     </div>
 </nav>
-
 </template>
 
 <script>
