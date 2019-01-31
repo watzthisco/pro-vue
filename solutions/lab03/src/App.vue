73ab2ac5ba1715c73b0fb94f5fe683e08082890c
Chris Minnick <chris@minnick.com>
2019-01-31 14:25:46 -0800

    updated lab03 to use bootstrap

---
+++ solutions/lab03/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hello, World!"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ solutions/lab03/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<nav class="navbar navbar-default">
+   <div class="container">
+      <a href="/" class="navbar-brand">Conduit</a>
+      <ul class="nav navbar-nav flex-row ">
+        <li class="nav-item p-2"><a class="nav-link">Home</a></li>
+        <li class="nav-item p-2"><a class="nav-link">Sign In</a></li>
+        <li class="nav-item p-2"><a class="nav-link">Sign Up</a></li>
+      </ul>
+    </div>
+</nav>
+
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
