22107c90217b78cbf2b75f96f2093304f1fc2c77
Chris Minnick <chris@minnick.com>
2019-02-11 14:14:43 -0800

    lab05

---
+++ solutions/lab05/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <div class="container">
+	conduit
+      <span class="attribution">
+		Code &amp; design licensed under MIT.
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
---
+++ solutions/lab05/src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home-page">
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+    </div>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="feed-toggle">
+            <ul class="nav nav-pills outline-active">
+              <li class="nav-item">Global Feed</li>   
+            </ul>
+          </div>
+        <p>Home Content Placeholder</p>
+        </div>
+        <div class="col-md-3">
+          <div class="sidebar">
+            <p>Popular Tags</p>
+            <div class="tag-list">
+                <ul class="tag-list">
+                    <li class="tag-default tag-pill tag-outline">
+dragons
+   </li>
+                    <li class="tag-default tag-pill tag-outline">
+sushi
+   </li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
