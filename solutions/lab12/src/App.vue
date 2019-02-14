bf2cf083caa5614a4b1a56ee27fe5f93b6229b0b
Chris Minnick <chris@minnick.com>
2019-02-14 15:58:06 -0800

    finishing lab12

---
+++ solutions/lab12/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <Home />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Home,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ solutions/lab12/src/components/ArticlePreview.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="article-preview">
+      <h1 v-text="article.title" />
+      <p v-text="article.description" />
+      <span>Read more...</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ArticlePreview',
+    props: {
+    article: { type: Object, required: true }
+    },
+}
+</script>
