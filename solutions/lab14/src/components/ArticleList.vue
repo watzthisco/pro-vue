15e911dbff5e5270dc3e8a81327c6c8cbbe54f22
Chris Minnick <chris@minnick.com>
2019-03-01 18:17:40 -0800

    working on lab14

---
+++ solutions/lab14/src/components/ArticleList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+      <div v-if="articles.length === 0" class="article-preview">
+        No articles are here... yet.
+      </div>
+      <ArticlePreview
+        v-for="(article, index) in articles"
+        :article="article"
+        :key="article.title + index"
+      />
+    </div>
+</template>
+
+<script>
+import ArticlePreview from './ArticlePreview.vue';
+import articles from '../json/articles.json';
+
+export default {
+    name: 'ArticleList',
+    components: {
+    ArticlePreview
+  },
+  data(){
+        return{
+            articles: articles
+        }
+    }
+}
+</script>
---
+++ solutions/lab14/src/components/ArticlePreview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="article-preview">
+      <h1 v-text="article.title" />
+      <p v-text="article.description" />
+      <span><a :href="articleLink.slug">Read more...</a></span>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ArticlePreview',
+    props: {
+      article: { type: Object, required: true }
+    },
+    computed: {
+      articleLink() {
+        return {
+          slug: this.article.slug
+        }
+      }
+    }
+}
+</script>
