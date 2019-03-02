69e3d0db4bc584fcf350def20ee9e75552346bae
Chris Minnick <chrisminnick@Chriss-Mac-mini.local>
2019-03-02 10:01:41 -0800

    finished lab14 solution

--- solutions/lab14/src/components/ArticlePreview.vue
+++ solutions/lab14/src/components/ArticlePreview.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="article-preview">
       <h1 v-text="article.title" />
+      <button
+        class="btn btn-sm float-right"
+        v-on:click="toggleFavorite"
+        :class="{
+          'btn-primary': favorited,
+          'btn-outline-primary': !favorited
+        }"
+      >
+        <i class="ion-heart"></i>
+      </button>
       <p v-text="article.description" />
       <span><a :href="articleLink.slug">Read more...</a></span>
   </div>
@@ -9,6 +19,11 @@
 <script>
 export default {
     name: 'ArticlePreview',
+    data: function(){
+      return {
+        favorited: false
+      }
+    },
     props: {
       article: { type: Object, required: true }
     },
@@ -17,6 +32,13 @@ export default {
         return {
           slug: this.article.slug
         }
+      }
+    },
+    methods: {
+      toggleFavorite() {
+        this.favorited = !this.favorited;
+        console.log("favorited = " + this.favorited);
+
       }
     }
 }
