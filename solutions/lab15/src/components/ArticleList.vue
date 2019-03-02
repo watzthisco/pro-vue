7c97e75744439755f8605cabf56678382c3e78b3
Chris Minnick <chrisminnick@Chriss-Mac-mini.local>
2019-03-02 10:41:43 -0800

    lab15

---
+++ solutions/lab15/src/components/ArticleList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+      <div v-if="articles.length === 0" class="article-preview">
+        No articles are here... yet.
+      </div>
+      <ArticlePreview
+        v-on:enlarge-text="articleFontSize += 0.1" 
+        :style="{ fontSize: articleFontSize + 'em' }"
+        v-for="(article, index) in articles"
+        :article="article"
+        :key="article.title + index"
+      />
+    </div>
+</template>
+
+<script>
+import ArticlePreview from './ArticlePreview.vue';
+import articles from '../json/articles.json';
+
+export default {
+    name: 'ArticleList',
+    components: {
+    ArticlePreview
+  },
+  data(){
+        return{
+			    articleFontSize:1,
+          articles: articles
+        }
+    }
+}
+</script>
---
+++ solutions/lab15/src/components/GlobalFeed.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-global"><ArticleList type="all" /></div>
+</template>
+
+<script>
+import ArticleList from './ArticleList.vue';
+
+export default {
+	name: 'GlobalFeed',
+	
+	components: {
+		ArticleList
+	}
+}
+</script>
